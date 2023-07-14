ff9bb0d4b69f8cbd495d150ce6eb20017675130d
Ozzy Ibrahim <ozzy.ibrahim@justice.com>
2023-07-14 11:37:19 +0100

    re-add removed var

--- components/access-packages/init.tf
+++ components/access-packages/init.tf
@@ -14,8 +14,9 @@ provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
 }
-# provider "azuread" {
-#   client_id     = var.client_id
-#   client_secret = var.client_secret
-#   tenant_id     = var.tenant_id
-# }
+
+provider "azuread" {
+  client_id     = var.client_id
+  client_secret = var.client_secret
+  tenant_id     = var.tenant_id
+}
