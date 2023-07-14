3a797ac878de15e5c85e8e3b5bc4e6ec1077516c
nicholas chang <nicholasc001@hotmail.com>
2023-07-14 14:59:55 +0100

    edit provider.tf

--- components/access-packages/init.tf
+++ components/access-packages/init.tf
@@ -12,13 +12,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-    client_credentials {
-      client_id     = var.client_id
-      client_secret = var.client_secret
-      tenant_id     = var.tenant_id
-    }
-  }
+  client_id     = var.client_id
+  client_secret = var.client_secret
+  tenant_id     = var.tenant_id
+
+  features {}
 }
 
 provider "azuread" {
