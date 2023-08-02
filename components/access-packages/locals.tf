fbc2e26465aa78934d8b64100d72cf26babfa3a5
Ozzy Ibrahim <ozzy.ibrahim@justice.com>
2023-08-02 15:56:04 +0100

    add some testing access packages

--- components/access-packages/locals.tf
+++ components/access-packages/locals.tf
@@ -15,7 +15,22 @@ locals {
     # Add more catalogs as needed
   ]
   common_tags = module.ctags.common_tags
+
+  packages = [
+    {
+      name          = "Test MI Data Platform"
+      description   = "Test MI Data Platform"
+      catalog_name  = "Test Data Platform"
+    },
+    {
+      name          = "Test SharedServices Storage Blob Data Reader"
+      description   = "Test SharedServices  Blob Data Reader Production Access"
+      catalog_name  = "Test Storage Blob Data Reader"
+    },
+    # Add more packages as needed
+  ]
 }
+
 
 # Common tags
 module "ctags" {
