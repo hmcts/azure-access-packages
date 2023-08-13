a94af9c416f24c83b8994e8627033957eb73efd9
felix.eyetan <felix.eyetan@justice.gov.uk>
2023-08-13 10:56:21 +0100

    Milstone: package resource association

--- components/access-packages/04-interpolated.tf
+++ components/access-packages/04-interpolated.tf
@@ -11,6 +11,33 @@ locals {
     ]
   ])
 
+  # Get the resource added to the package from the catalog resource list
+  packages_resource_roles = flatten([
+    for package in local.packages : [
+      for catalog in local.catalogs : [
+        for resource in try(catalog.resources, []) : {
+          name                 = package.name
+          resource_association = "${catalog.name}:${resource}"
+        } if contains(try(package.resource_roles, []),resource)
+      ] if package.catalog_name == catalog.name
+    ]
+  ])
+
+  package_policies = flatten([
+    for policy_file in fileset(path.module, "../../package-policies/*.yml") : [
+      yamldecode(file(policy_file))
+    ]
+  ])
+
+  package_assignment_policy = flatten([
+    for package_policy in local.package_policies : [
+      for policy in try(package_policy.policies, []) : {
+        access_package = package_policy.access_package
+        policy         = policy
+      }
+    ]
+  ])
+
   common_tags = module.ctags.common_tags
 }
 
--- components/access-packages/07-packages.tf
+++ components/access-packages/07-packages.tf
@@ -1,7 +1,9 @@
-/*
- * Package entries are defined in the entitlement-packages.yml file
- * No need to make any updates here for new entries
- */
+# --------------------------------------------------------------------
+# Package entries are defined in the entitlement-packages.yml file
+# No need to make any updates here for new entries
+# --------------------------------------------------------------------
+
+# ------- Packages --------- #
 resource "azuread_access_package" "package" {
   for_each = {
     for package in local.packages : package.name => package
@@ -13,4 +15,59 @@ resource "azuread_access_package" "package" {
   depends_on = [
     azuread_access_package_catalog.catalog
   ]
+}
+
+# ------------------------ RESOURCES ROLES -------------------------------
+# Resources from the catalog that are added to the access package
+# Either a Group and Teams, Application or Sharepoint site type resource
+# ------------------------------------------------------------------------
+
+resource "azuread_access_package_resource_package_association" "this" {
+  for_each = {
+    for resource in local.packages_resource_roles : "${resource.name}:${resource.resource_association}" => resource
+  }
+  access_package_id               = azuread_access_package.package[each.value.name].id
+  catalog_resource_association_id = azuread_access_package_resource_catalog_association.this[each.value.resource_association].id
+}
+
+
+# ------------------------ PACKAGE POLICIES -------------------------------
+# The policy defined as part of the access package
+# -------------------------------------------------------------------------
+
+resource "azuread_access_package_assignment_policy" "this" {
+  for_each = {
+    for idx, policy in local.package_assignment_policy : format("%02s:%s", idx, policy.access_package) => policy
+  }
+  access_package_id = azuread_access_package.package[each.value.access_package].id
+  display_name      = each.value.policy.display_name
+  description       = each.value.policy.description
+  duration_in_days  = try(each.value.duration_in_days, null)
+  expiration_date   = try(each.value.expiration_date, null)
+
+  dynamic "approval_settings" {
+    for_each = try(each.value.policy.approval_settings, null) != null ? var.placeholder : {}
+    content {
+      approval_required_for_extension = try(each.value.policy.approval_required_for_extension, null)
+      approval_required               = try(each.value.policy.approval_required, null)
+
+      dynamic "approval_stage" {
+        for_each = try(each.value.policy.approval_settings.approval_stage, null) != null ? var.placeholder : {}
+        content {
+          approval_timeout_in_days     = try(each.value.policy.approval_settings.approval_stage.approval_timeout_in_days, null)
+          alternative_approval_enabled = try(each.value.policy.approval_settings.approval_stage.alternative_approval_enabled, null)
+
+          dynamic "alternative_approver" {
+            for_each = try(each.value.policy.approval_settings.approval_stage.alternative_approver, null) != null ? var.placeholder : {}
+            content {
+              backup       = try(each.value.policy.approval_settings.approval_stage.alternative_approver.backup, null)
+              object_id    = try(each.value.policy.approval_settings.approval_stage.alternative_approver.object_id, null)
+              subject_type = each.value.policy.approval_settings.approval_stage.alternative_approver.subject_type
+            }
+          }
+        }
+      }
+      requestor_justification_required = try(each.value.policy.requestor_justification_required, null) # (Optional) Whether a requestor is required to provide a justification to request an access package. Justification is visible to approvers and the requestor.
+    }
+  }
 }
