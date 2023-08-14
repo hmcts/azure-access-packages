761d14aada19e29cc498a42f9e8efb73cb2a4407
felix.eyetan <felix.eyetan@justice.gov.uk>
2023-08-14 15:15:55 +0100

    Reviews Update: Refactored code, removed dub and simplified

--- components/access-packages/04-interpolated.tf
+++ components/access-packages/04-interpolated.tf
@@ -11,32 +11,6 @@ locals {
     ]
   ])
 
-  # If catalog has users defined
-  catalog_role_assignment_users = flatten([
-    for catalog in local.catalogs : [
-      for user in try(catalog.roles.users, []) : {
-        catalog_name = catalog.name
-        user         = user
-        assignee = join(".", [
-          split(".", split("@", user.user_principal_name)[0])[0],
-          substr((split(".", split("@", user.user_principal_name)[0])[1]), 0, 1)
-        ])
-        # Just dont want emails being logged in statefile
-        # "firstname.lastname@justice.gov.uk" becomes firstname.l
-      }
-    ]
-  ])
-
-  # If catalog has groups defined
-  catalog_role_assignment_groups = flatten([
-    for catalog in local.catalogs : [
-      for group in try(catalog.roles.groups, []) : {
-        catalog_name = catalog.name
-        group        = group
-      }
-    ]
-  ])
-
   common_tags = module.ctags.common_tags
 }
 
--- components/access-packages/06-catalogs.tf
+++ components/access-packages/06-catalogs.tf
@@ -21,19 +21,19 @@ resource "azuread_access_package_catalog" "catalog" {
 
 data "azuread_group" "resources" {
   for_each = {
-    for item in local.catalogs_resources : "${item.catalog_name}:${item.resource.display_name}" => item
+    for item in local.catalogs_resources : "${item.catalog_name}:${item.resource}" => item
   }
-  display_name     = each.value.resource.display_name
-  security_enabled = try(each.value.security_enabled, null)
+  display_name     = each.value.resource
+  security_enabled = true
 }
 
 resource "azuread_access_package_resource_catalog_association" "this" {
   for_each = {
-    for item in local.catalogs_resources : "${item.catalog_name}:${item.resource.display_name}" => item
+    for item in local.catalogs_resources : "${item.catalog_name}:${item.resource}" => item
   }
   catalog_id             = azuread_access_package_catalog.catalog[each.value.catalog_name].id
-  resource_origin_id     = data.azuread_group.resources["${each.value.catalog_name}:${each.value.resource.display_name}"].object_id
-  resource_origin_system = each.value.resource.resource_origin_system
+  resource_origin_id     = data.azuread_group.resources["${each.value.catalog_name}:${each.value.resource}"].object_id
+  resource_origin_system = "AadGroup"
 }
 
 # --------------------- ROLES AND ADMINISTRATORS -------------------------
@@ -43,50 +43,21 @@ resource "azuread_access_package_resource_catalog_association" "this" {
 
 # ------- Owner types ------- #
 data "azuread_access_package_catalog_role" "this" {
-  for_each     = toset(["Catalog owner", "Catalog creator"])
-  display_name = each.key
-}
-
-# ------- user_principal_name must be specified ------- #
-data "azuread_user" "this" {
-  for_each = {
-    for item in local.catalog_role_assignment_users : "${item.catalog_name}:${item.assignee}" => item
-  }
-
-  user_principal_name = each.value.user.user_principal_name
-  object_id           = try(each.value.user.object_id, null)
-  employee_id         = try(each.value.user.employee_id, null)
-  mail                = try(each.value.user.mail, null)
-  mail_nickname       = try(each.value.user.mail_nickname, null)
-}
-
-# ------- Resources roles and administrators: Users ------- #
-resource "azuread_access_package_catalog_role_assignment" "users" {
-  for_each = {
-    for item in local.catalog_role_assignment_users : "${item.catalog_name}:${item.assignee}" => item
-  }
-  role_id             = data.azuread_access_package_catalog_role.this[each.value.user.catalog_role].object_id
-  principal_object_id = data.azuread_user.this["${each.value.catalog_name}:${each.value.assignee}"].object_id
-  catalog_id          = azuread_access_package_catalog.catalog[each.value.catalog_name].id
+  display_name = "Catalog owner"
 }
 
 # ------- display_name must be specified ------- #
 data "azuread_group" "this" {
-  for_each = {
-    for group in local.catalog_role_assignment_groups : "${group.catalog_name}:${group.group.display_name}" => group
-  }
-  display_name     = each.value.group.display_name
-  mail_enabled     = try(each.value.group.mail_enabled, null)
-  object_id        = try(each.value.group.object_id, null)
-  security_enabled = try(each.value.group.security_enabled, null)
+  display_name     = "DTS Platform Operations"
+  security_enabled = true
 }
 
 # ------- Resources roles and administrators: Groups ------- #
 resource "azuread_access_package_catalog_role_assignment" "groups" {
   for_each = {
-    for item in local.catalog_role_assignment_groups : "${item.catalog_name}:${item.group.display_name}" => item
+    for catalog in local.catalogs : catalog.name => catalog
   }
-  role_id             = data.azuread_access_package_catalog_role.this[each.value.group.catalog_role].object_id
-  principal_object_id = data.azuread_group.this["${each.value.catalog_name}:${each.value.group.display_name}"].object_id
-  catalog_id          = azuread_access_package_catalog.catalog[each.value.catalog_name].id
+  role_id             = data.azuread_access_package_catalog_role.this.object_id
+  principal_object_id = data.azuread_group.this.object_id
+  catalog_id          = azuread_access_package_catalog.catalog[each.value.name].id
 }
