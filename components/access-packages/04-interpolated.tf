babe7248051aea0a25d9f3b132b92763a9f15752
felix.eyetan <felix.eyetan@justice.gov.uk>
2023-10-23 15:40:59 +0100

    Fixing merge conflict issues

--- components/access-packages/04-interpolated.tf
+++ components/access-packages/04-interpolated.tf
@@ -31,14 +31,12 @@ locals {
 
   package_assignment_policy = flatten([
     for package in local.packages : [
-      for policy in package.policies : [
-        for policy_definition in local.policy_definitions : {
-          access_package = package.name
-          policy_name    = policy_definition.name
-          policy         = policy_definition.policy
-        } if policy == policy_definition.name
-      ]
-    ] if try(package.policies, null) != null
+      for package_policy in local.package_policies : {
+        access_package = package.name
+        policy_name    = package_policy.name
+        policy         = package_policy.policy
+      } if package.policy == package_policy.name
+    ] if try(package.policy, null) != null
   ])
 
   common_tags = module.ctags.common_tags
