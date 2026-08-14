b8a36e37db190edda2f69618f1db6d6cc8941895
adusumillipraveen <praveen.adusumilli@hmcts.net>
2026-08-14 09:55:12 +0100

    Use debug terraform library branch

--- components/access-packages/init.tf
+++ components/access-packages/init.tf
@@ -27,11 +27,5 @@ provider "azuread" {
 }
 
 provider "msgraph" {
-  client_id      = var.client_id
-  client_secret  = var.client_secret
-  tenant_id      = var.tenant_id
-  use_cli        = false
-  use_msi        = false
-  use_oidc       = false
-  use_powershell = false
+  use_cli = false
 }
